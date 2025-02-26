cc1bdb2802bc235b5829b4a410ea1192d5674a91
Agustin Jalil <agustinjalilleon@gmail.com>
2025-02-26 09:53:52 -0300

    adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,16 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: "server",
+
   devToolbar: {
     enabled: false
-  }
+  },
 
+  adapter: netlify()
 });
